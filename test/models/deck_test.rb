b61cb877968dc0cb468566ffe4755306903f3160
Zach Baxter <zbax777@gmail.com>
2016-02-02 15:12:23 -0600

    made tests for deck model

--- test/models/deck_test.rb
+++ test/models/deck_test.rb
@@ -1,7 +1,31 @@
 require 'test_helper'
 
 class DeckTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "creates a valid deck" do
+    deck = Deck.new
+    deck.title = "Test Deck"
+    deck.author = "Zach"
+    deck.category = "cats"
+    
+    assert deck.save
+  end
+
+  test "should not save unless title is filled in" do
+    deck = Deck.new
+    assert !deck.save
+  end
+
+  test "should not save unless Author is filled in" do
+    deck = Deck.new
+    deck.title = "new deck"
+    assert !deck.save
+  end
+
+  test "should only have one deck with the same name" do
+    deck = Deck.new(:title =>"Test")
+    deck.valid?
+    assert deck.errors[:title].any?
+  end
+
 end
+
