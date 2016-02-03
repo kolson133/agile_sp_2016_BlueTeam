06961179323419287cab660d751623a1556b594e
Lance Matysik <lancematysik@gmail.com>
2016-02-02 23:13:46 -0600

    generated scaffold for cards

---
+++ app/views/cards/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @card, :id, :question, :answer, :deck_id, :created_at, :updated_at
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :cards
   root controller: 'home', action: 'index'
 
   resources :decks
