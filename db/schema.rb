62f859b91b97208977622f9aa8ac8cc7531f21be
Kevin Olson <achilles2865@gmail.com>
2016-02-04 22:49:44 +0000

    User font changes and its test

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160204065627) do
+ActiveRecord::Schema.define(version: 20160204201757) do
 
   create_table "cards", force: :cascade do |t|
     t.string   "question"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20160204065627) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "color_type"
+    t.string   "font_type"
   end
 
 end
