c564f34e431a42a95ae1daaa5afb7c8b9d7d5330
Kevin Olson <achilles2865@gmail.com>
2016-02-09 04:42:51 +0000

    Change links to be more user friendly

--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -29,7 +29,7 @@ class DecksController < ApplicationController
 
     respond_to do |format|
       if @deck.save
-        format.html { redirect_to @deck, notice: 'Deck was successfully created.' }
+        format.html { redirect_to decks_path, notice: 'Deck was successfully created.' }
         format.json { render :show, status: :created, location: @deck }
       else
         format.html { render :new }
