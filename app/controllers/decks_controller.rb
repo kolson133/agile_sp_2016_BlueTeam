d24004bb68728dbc7935dcfb14f7fb73064befc8
kolson133 <achilles2865@gmail.com>
2016-02-15 02:59:23 +0000

    Add foreground changes for deck

--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -79,6 +79,6 @@ class DecksController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def deck_params
 
-      params.require(:deck).permit(:title, :author, :description, :category, :color_type, :font_type)
+      params.require(:deck).permit(:title, :author, :description, :category, :color_type, :font_type, :foreground_color)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160213235335) do
+ActiveRecord::Schema.define(version: 20160215025505) do
 
   create_table "cards", force: :cascade do |t|
     t.string   "question"
@@ -38,11 +38,13 @@ ActiveRecord::Schema.define(version: 20160213235335) do
     t.string   "title"
     t.string   "author"
     t.string   "category"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
     t.string   "color_type"
     t.string   "font_type"
     t.string   "description"
+    t.string   "foreground"
+    t.string   "foreground_color"
   end
 
 end
