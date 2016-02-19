747bd89d3bd6e594deb91fd8ae76dfd6c49a1d13
Lance Matysik <lancematysik@gmail.com>
2016-02-19 14:59:14 -0600

    more bugfixes regarding omniauth
    its because facebook twitter aren't going to play kindly as twitter doesn't provide an email address

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,13 +5,11 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :omniauth_providers => [:facebook, :twitter]
 
   def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_initialize.tap do |user|
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.provider = auth.provider
       user.uid = auth.uid
-      user.email = auth.info.email
       user.name = auth.info.name
       user.password = Devise.friendly_token[0,20]
-      user.save!
     end
   end
 end
