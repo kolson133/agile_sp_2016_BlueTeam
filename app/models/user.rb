d8382d550eea04404e7d799a6828b7581f0dedf7
Lance Matysik & Kevin Olson <lancematysik@gmail.com>
2016-02-21 01:31:16 -0600

    fix to make google play well with other social media logins

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,25 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :omniauth_providers => [:facebook, :twitter, :google_oauth2]
-
   def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.provider = auth.provider
-      user.uid = auth.uid
-      user.name = auth.info.name
-      user.password = Devise.friendly_token[0,20]
+
+    if auth.provider == "twitter" || auth.provider == "facebook"
+      where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+        user.provider = auth.provider
+        user.uid = auth.uid
+        user.name = auth.info.name
+        user.password = Devise.friendly_token[0,20]
+      end
+    else
+      data = auth.info
+      user = User.where(:email => data["email"]).first
+      unless user
+        user = User.create(name: data["name"],
+          email: data["email"],
+          password: Devise.friendly_token[0,20],
+          provider: "Google")
+      end
+      user
     end
   end
 end
